56d6e95f4dab448703c4e2aefe64f1b93fc2590b
taku <kosmo.waizu0804@gmail.com>
2017-08-18 13:38:28 +0900

    9.1までの演習問題完

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,21 +10,25 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
-  def User.digest(string)
+  # 渡された文字列のハッシュ値を返す
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                               BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
-  def User.new_token
+  # ランダムなトークンを返す
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
+  # 永続セッションのためにユーザーをデータベースに記憶する
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
