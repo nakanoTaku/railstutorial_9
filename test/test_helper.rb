27247fdc6a8473ebd6aa7e749520678fcb808094
taku <kosmo.waizu0804@gmail.com>
2017-08-21 13:07:12 +0900

    演習問題をやったー

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,10 +10,12 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 
+  # テストユーザーがログイン中の場合にtrueを返す
   def is_logged_in?
     !session[:user_id].nil?
   end
 
+  # テストユーザーとしてログインする
   def log_in_as(user)
     session[:user_id] = user.id
   end
